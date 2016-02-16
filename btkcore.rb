190186fcfcc69a3c065cea5e6c5365528229cd1d
Shrinidhi KL <klshrinidhi@gmail.com>
2016-02-15 16:24:55 -0800

    Bug fix: missing commas.

--- btkcore.rb
+++ btkcore.rb
@@ -6,26 +6,26 @@ class Btkcore < Formula
            "BTKCore-0.3.01.tar.gz"
   sha256   "cadf08e126d23f45327d1faf958082788cc523068cdf9a583841c5229728c286"
 
-  option "with-btype-Release"        "\n\tSet build-type to Release [Default]."
-  option "with-btype-RelWithDebInfo" "\n\tSet build-type to RelWithDebInfo."
-  option "with-btype-Debug"          "\n\tSet build-type to Debug."
+  option "with-Release"       , "Set build-type to Release."
+  option "with-RelWithDebInfo", "Set build-type to RelWithDebInfo."
+  option "with-Debug"         , "Set build-type to Debug."
 
   depends_on "cmake" => :build
 
   def install
     Dir.mkdir "build"
     Dir.chdir "build"
+    build_type = "Release"
     if build.with? "RelWithDebInfo"
-      cmake_build_type = "RelWithDebInfo"
-    elsif build.with? "Debug"
-      cmake_build_type = "Debug"
-    else
-      cmake_build_type = "Release"
+      build_type = "RelWithDebInfo"
+    end
+    if build.with? "Debug"
+      build_type = "Debug"
     end
     system "cmake", "..",
            "-DBUILD_SHARED_LIBS=ON",
            "-DCMAKE_INSTALL_PREFIX=#{prefix}",
-           "-DCMAKE_BUILD_TYPE=#{cmake_build_type}"
+           "-DCMAKE_BUILD_TYPE=#{build_type}"
     system "make", "install"
   end
 end
